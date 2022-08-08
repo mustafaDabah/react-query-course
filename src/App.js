18e855e0e4ad42a91369c434d69bcda90c3d2606
mustafaDabah <mustafadabah5555@gmail.com>
2022-08-08 22:27:47 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-import PostsList from "./features/posts/PostsList";
-import AddPostForm from "./features/posts/AddPostForm";
-import SinglePostPage  from "./features/posts/SinglePostPage";
-import { Route, Routes , Navigate } from "react-router-dom";
-import Layout from "./Components/Layout";
-import EditPostForm from "./features/posts/EditPostForm";
-import UsersList from "./features/users/UserList";
-import UserPage , {UserPageMemo} from "./features/users/UserPage";
+import TodoList from "./features/todos/TodoList";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<PostsList />} />
-        <Route path="post">
-          <Route index element={<AddPostForm />} />
-          <Route path=":postId" element={<SinglePostPage />} />
-          <Route path="edit/:postId" element={<EditPostForm/>} />
-        </Route>
-
-        <Route  path="user">
-          <Route index element={<UsersList/>} />
-          <Route path=":userId" element={<UserPage/>} />
-        </Route>
-
-        <Route path="*" element={<Navigate to='/' replace />} />
-      </Route>
-    </Routes>
-  );
+  return <TodoList />
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { store } from "./app/store";
-import { Provider } from "react-redux";
-import { fetchUsers } from "./features/users/usersSlice";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { fetchPosts } from "./features/posts/postsSlice";
+import { ApiProvider } from "@reduxjs/toolkit/query/react";
+import { apiSlice } from "./features/todos/api/apiSlice";
 
-store.dispatch(fetchPosts());
-store.dispatch(fetchUsers());
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
-      </Router>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ApiProvider api={apiSlice}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApiProvider>
 );
